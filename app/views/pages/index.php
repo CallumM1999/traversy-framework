cd49a4e7ceea35c109f40d21d146e93bf6ef472a
Callum <callummac@protonmail.com>
2019-10-03 18:17:19 +0100

    initial commit

---
+++ app/views/pages/index.php
@@ -0,0 +1,11 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<h1>
+    <?= $data['title'] ?>
+</h1>
+
+<p>This is the TraversyMCV PHP framework. Please refer to the docs on how to use it</p>
+
+<hr>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
